868e03e09b447dc5dffa9033202c8d77e9d37d4b
Oguri Cap <130316718+Just-Oguri-Cap@users.noreply.github.com>
2026-02-20 19:02:26 -0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.singleton_0602_rpg {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.singleton_0602_rpg to javafx.fxml;
-    exports org.example.singleton_0602_rpg;
-}
--- src/main/java/org/example/singleton_0602_rpg/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.singleton_0602_rpg;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-
-    }
-}
